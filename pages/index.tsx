71bd807696af532d17a7a3a65c381d83f691157c
Dominic Camarco <doccamarco@gmail.com>
2023-02-18 08:48:53 -0700

    Added Hero Section and some responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,36 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
+import HeroBear from "../public/HeroImageHQ.png";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
 		<>
-			<h1 className='text-5xl text-rose-700 text-center'>Hello</h1>
-			<div className='bg'></div>
+			<section className='relative w-screen lg:h-[729px]'>
+				<Image src={HeroBear} alt='hero image, bear/honeypot' />
+				<div
+					className='
+				absolute top-0 left-0 
+				// Large Screen
+				lg:ml-[240px] lg:mr-[1002] lg:mt-[238px] lg:mb-[286px] lg:w-[640px] lg:h-[20%] 
+				// Medium Screen
+				md:ml-[82px] md:mr-[437] md:my-[96px] md:w-[301px] md:h-[122] 
+				//Small Screen
+				flex justify-center items-center'
+				>
+					<h2 className=' text-white text-1xl md:text-2xl lg:text-6xl'>
+						Transforming the way Arrcaia interacts with Clients
+					</h2>
+				</div>
+			</section>
+			<section></section>
+			<section></section>
+			<section></section>
+			<section></section>
+			<section></section>
+			<section></section>
 		</>
 	);
 }
